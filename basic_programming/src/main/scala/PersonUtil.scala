6575452b5cff4a443a300ef67f112f6b182fd44f
久保 和貴 <k-kubo@biglobe.co.jp>
2013-06-21 21:42:13 +0900

    解答追加。 Signed-off-by: 久保 和貴 <k-kubo@biglobe.co.jp>

--- basic_programming/src/main/scala/PersonUtil.scala
+++ basic_programming/src/main/scala/PersonUtil.scala
@@ -12,6 +12,17 @@ object PersonUtil {
 
   // 問題9.7: personリストから、血液型がAの人数を返す関数
   def count_ketsueki_A(list: List[Person]): Int = {
-    1
+    list match {
+      case Nil => 0
+      case _ => list.head.bloodType match {
+        case "A" => 1 + count_ketsueki_A(list.tail)
+        case _ => 0 + count_ketsueki_A(list.tail)
+      }
+    }
+  }
+
+  // 問題9.8: 改変。体重が65の人の名前のリストを返す関数
+  def sixtyfive(list: List[Person]): List[String] = {
+    list.filter( p => p.weight == 65).map( p => p.name)
   }
 }
