3db88ad0a78bb59aa1fb0dbfc0bc16ae0045aafb
久保 和貴 <k-kubo@biglobe.co.jp>
2013-09-24 17:36:20 +0900

    p108の問題を追加。 Signed-off-by: 久保 和貴 <k-kubo@biglobe.co.jp>

--- latticepoint/src/main/scala/LatticePoint.scala
+++ latticepoint/src/main/scala/LatticePoint.scala
@@ -13,7 +13,7 @@ class LatticePoint(p1: MyPoint, p2: MyPoint) {
   // p1,p2を除いた、p1p2上の格子点の数を返す
   def solve: Int = {
     val absX = (p1.x - p2.x).abs
-    val absY = (p2.y - p2.y).abs
+    val absY = (p1.y - p2.y).abs
 
     Euclid.euclid(absX, absY) - 1
   }
---
+++ sugoroku/src/main/scala/App.scala
@@ -0,0 +1,7 @@
+package org.assaulter.sugoroku
+
+object App {
+  def main(args: Array[String]) {
+    print("Hello org.assaulter.sugoroku!")
+  }
+}
